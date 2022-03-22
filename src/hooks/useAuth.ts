a98d15333d3bcf89763dc98b76f51724aa2056d2
okabeeeat <sadayasu.info@gmail.com>
2022-03-22 16:54:20 +0900

    ログインユーザー情報をContextに保持してみる

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useCallback, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { User } from '../types/api/user';
+import { useLoginUser } from './useLoginUser';
 import { useMessage } from './useMessage';
 
 export const useAuth = () => {
@@ -9,6 +10,7 @@ export const useAuth = () => {
   const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
   // 注) ここ(login関数の外)にsetLoading(true);を書くと Too many re-renders が発生する
+  const { setLoginUser } = useLoginUser();
 
   const login = useCallback(
     (id: string) => {
@@ -18,19 +20,21 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: 'ログインに成功しました', status: 'success' });
             history.push('/home');
           } else {
             showMessage({ title: 'ユーザーが見つかりません', status: 'error' });
             alert('ユーザーが見つかりません');
+            setLoading(false);
           }
         })
-        .catch(() =>
-          showMessage({ title: 'ログインできません', status: 'error' }),
-        )
-        .finally(() => setLoading(false));
+        .catch(() => {
+          showMessage({ title: 'ログインできません', status: 'error' });
+          setLoading(false);
+        });
     },
-    [history, showMessage],
+    [history, showMessage, setLoginUser],
   );
   return { login, loading };
 };
