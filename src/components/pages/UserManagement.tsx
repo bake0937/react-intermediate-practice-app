2e7bb08192d5c4108c82f6e63a1d21c49fbc7ad9
bake0937 <sadayasu.info@gmail.com>
2022-03-22 14:55:36 +0900

    ユーザー詳細モーダル機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -7,17 +7,24 @@ import {
 } from '@chakra-ui/react';
 import { memo, useCallback, useEffect, VFC } from 'react';
 import { useAllUsers } from '../../hooks/useAllUsers';
+import { useSelectUser } from '../../hooks/useSelectUser';
 import { UserCard } from '../organisms/user/UserCard';
 import { UserDetailModal } from '../organisms/user/UserDetailModal';
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
 
   // コンポーネントをマウント時([]に何も指定しない)にユーザー一覧を取得する
   useEffect(() => getUsers(), []);
 
-  const onClickUser = useCallback(() => onOpen(), []);
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen });
+    },
+    [users, onSelectUser, onOpen],
+  );
   return (
     <>
       {loading ? (
@@ -29,6 +36,7 @@ export const UserManagement: VFC = memo(() => {
           {users.map((user) => (
             <WrapItem key={user.id} mx='auto'>
               <UserCard
+                id={user.id}
                 imageUrl='https://source.unsplash.com/random'
                 userName={user.username}
                 fullName={user.name}
@@ -38,7 +46,7 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
