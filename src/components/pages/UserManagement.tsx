024a68af1e29f694fac9c5e227fddb8fbcb41ed7
bake0937 <sadayasu.info@gmail.com>
2022-03-22 12:39:58 +0900

    ユーザー詳細モーダル画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,32 @@
-import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
-import { memo, useEffect, VFC } from 'react';
+import {
+  Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Stack,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
+import { memo, useCallback, useEffect, VFC } from 'react';
 import { useAllUsers } from '../../hooks/useAllUsers';
 import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
 
   // コンポーネントをマウント時([]に何も指定しない)にユーザー一覧を取得する
   useEffect(() => getUsers(), []);
 
+  const onClickUser = useCallback(() => onOpen(), []);
   return (
     <>
       {loading ? (
@@ -23,11 +41,44 @@ export const UserManagement: VFC = memo(() => {
                 imageUrl='https://source.unsplash.com/random'
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset='slideInBottom'
+      >
+        <ModalOverlay />
+        <ModalContent pb={6}>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value='okabeeeat' isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value='oka beeeat' isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>MAIL</FormLabel>
+                <Input value='sample@gmail.com' isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>TEL</FormLabel>
+                <Input value='090-1111-2222' isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 });
