426980ab0fbacf9d2ff79f184323599430d03089
bake0937 <sadayasu.info@gmail.com>
2022-03-22 10:59:57 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,36 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react';
 import { memo, VFC } from 'react';
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w='260px'
+          h='260px'
+          bg='white'
+          borderRadius='10px'
+          shadow='md'
+          p={4}
+          _hover={{ cursor: 'pointer', opacity: 0.8 }}
+        >
+          <Stack textAlign='center'>
+            <Image
+              borderRadius='full'
+              boxSize='160px'
+              src='https://source.unsplash.com/random'
+              alt='プロフィール画像'
+              margin='auto'
+            ></Image>
+            <Text fontSize='lg' fontWeight='bold'>
+              okabeeeat
+            </Text>
+            <Text fontSize='sm' color='gray'>
+              okabeeeat okabeeeat
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
